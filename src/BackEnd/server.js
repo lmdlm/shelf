e9407797f76c6af82bafe99d0beb0abaa496a940
lmdlm <g00363430@gmit.ie>
2019-12-01 19:53:37 +0000

    final commit

--- src/BackEnd/server.js
+++ src/BackEnd/server.js
@@ -95,7 +95,7 @@ app.get('/api/books/:id', (req, res) => {
 
 const userSchema = new Schema({
     username: String,
-    email: Strng,
+    email: String,
 })
 
 const UserModel = mongoose.model('user', userSchema);
